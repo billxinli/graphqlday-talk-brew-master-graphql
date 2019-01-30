9e627cc08a6d1406e1c5e130401850c51c96be19
Bill Li <me@billxinli.com>
2019-01-29 22:48:10 -0500

    wip

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
-const env = process.env.NODE_ENV || 'production'
-module.exports = {
-  ...require(`./${env}.js`),
-  env
-}
+/* eslint-disable import/no-dynamic-require */
+const env = process.env.NODE_ENV || 'development'
+const config = require(`./${env}`)
+config.env = env
+
+module.exports = config
